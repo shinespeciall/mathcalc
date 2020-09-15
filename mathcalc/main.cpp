257a116742ac9602ab6f9da3a09ff3ac176faa82
Chen Yifeng <648616375@qq.com>
2020-09-15 20:40:47 +0800

    add eigen337 build and test code

--- mathcalc/main.cpp
+++ mathcalc/main.cpp
@@ -1,10 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
-
+#include <iostream>
+#include <Eigen/Dense>
 #include "Steffensen.h"
+
+using Eigen::MatrixXd;
 
 void main()
 {
-	steffensen();
+	// steffensen();
+	MatrixXd m(2, 2);
+	m(0, 0) = 3;
+	m(1, 0) = 2.5;
+	m(0, 1) = -1;
+	m(1, 1) = m(1, 0) + m(0, 1);
+	std::cout << m << std::endl;
 	return;
 }
